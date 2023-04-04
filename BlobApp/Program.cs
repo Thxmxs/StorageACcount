a10bbfdc0d2e86017ee4296cba2bf6f0d9a68e18
Thxmxs <thomas.c.arcos@gmail.com>
2023-04-04 00:39:02 -0400

    tableStorage

--- BlobApp/Program.cs
+++ BlobApp/Program.cs
@@ -1,142 +1,81 @@
 ﻿using Azure;
-using Azure.Storage.Blobs;
-using Azure.Storage.Blobs.Models;
-using Azure.Storage.Blobs.Specialized;
+using Azure.Data.Tables;
+using BlobApp;
 
+string connectionString = "ConnectionString";
+string tableName = "Orders";
 
-
-//CREAR UN CONTAINER
-
-
-string connectionString = "DefaultEndpointsProtocol=https;AccountName=appstore20237;AccountKey=Lsk9U/W+AyCq/x2TEqnJ1A81O3/vnGtSKlfIKQmbvA4quEU2xXilwGWdiGCciEa2CZftHPsiJIs+AStsLrAQQ==;EndpointSuffix=core.windows.net";
-string containerName = "scripts";
-
-BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
-
-await blobServiceClient.CreateBlobContainerAsync(containerName, Azure.Storage.Blobs.Models.PublicAccessType.Blob);
-
-Console.WriteLine("Container created");
-
- 
-
-
-//SUBIR UN BLOB A UN CONTAINER
-
-
-string connectionString = "DefaultEndpointsProtocol=https;AccountName=appstore20237;AccountKey=Lsk9U/W+AyCqq/x2TEqnJ1A81O3/vnGtSKlfIKQmbvA4quEU2xXilwGWdiGCciEa2CZftHPsiJIs+AStsLrAQQ==;EndpointSuffix=core.windows.net";
-string containerName = "scripts";
-string blobName = "avatar22.png";
-string filePath = "C:\\tmp\\avatar22.png";
-
-BlobContainerClient blobServiceClient = new BlobContainerClient(connectionString,containerName);
-
-BlobClient blobClient = blobServiceClient.GetBlobClient(blobName);
-await blobClient.UploadAsync(filePath,true);
-
-blobServiceClient.GetBlobClient(blobName);
-
-Console.WriteLine("Blob Uploaded");
+//instalar azure.data.Tables
 
 
 
 
-//LISTAR BLOBS DE UN CONTAINER
 
+//AÑADIR ENTIDADES A UN TABLE
 
-string connectionString = "DefaultEndpointsProtocol=https;AccountName=appstore20237;AccountKey=Lsk9U/W+AyCqq/x2TEqnJ1A81O3/vnGtSKlfIKQmbvA4quEU2xXilwGWdiGCciEa2CZftHPsiJIs+AStsLrAQQ==;EndpointSuffix=core.windows.net";
-string containerName = "scripts";
-BlobContainerClient blobContainerClient = new BlobContainerClient(connectionString, containerName);
-
-await foreach (var blobItem in blobContainerClient.GetBlobsAsync())
+void AddEntity(string OrderId,string category, int quantity)
 {
-    Console.WriteLine("The BLOB Name is {0}", blobItem.Name);
-    Console.WriteLine("Blob Properties is {0}",blobItem.Properties);
-}
+    TableClient tableClient = new TableClient(connectionString,tableName);
 
-
-
-//DESCARGAR UN BLOB
-
-
-string connectionString = "DefaultEndpointsProtocol=https;AccountName=appstore20237;AccountKey=Lsk9U/W+AyCqq/x2TEqnJ1A81O3/vnGtSKlfIKQmbvA4quEU2xXilwGWdiGCciEa2CZftHPsiJIs+AStsLrAQQ==;EndpointSuffix=core.windows.net";
-string containerName = "scripts";
-string blobName = "avatar22.png";
-string filePath = "C:\\tmp\\avatar22.png";
-
-BlobClient blobClient = new BlobClient(connectionString, containerName, blobName);
-
-await blobClient.DownloadToAsync(filePath);
-
-Console.WriteLine("The blob is downloaded");
-
-
-
-
-
-//ADD METADATA TO A BLOB
-
-
-string connectionString = "DefaultEndpointsProtocol=https;AccountName=appstore20237;AccountKey=Lsk9U/W+AyCqq/x2TEqnJ1A81O3/vnGtSKlfIKQmbvA4quEU2xXilwGWdiGCciEa2CZftHPsiJIs+AStsLrAQQ==;EndpointSuffix=core.windows.net";
-string containerName = "scripts";
-
-await SetBlobMetaData();
-
-async Task SetBlobMetaData(){
-
-    string blobName = "avatar22.png";
-
-    BlobClient blobClient = new BlobClient(connectionString, containerName, blobName);
-
-    IDictionary<string,string> metaData = new Dictionary<string,string>();
-
-    metaData.Add("Type", "Image");
-    metaData.Add("Application", "BlobApp");
-
-    await blobClient.SetMetadataAsync(metaData);
-
-    Console.WriteLine("Metadata added");
-
-}
-
-//OBTENER METADATA DE UN ARCHIVO
-
-
-await getMetadata();
-
-async Task getMetadata()
-{
-    string blobName = "avatar22.png";
-    BlobClient blobClient = new BlobClient(connectionString, containerName, blobName);
-
-    BlobProperties blobProperties = await blobClient.GetPropertiesAsync();
-
-    foreach (var item in blobProperties.Metadata)
+    TableEntity tableEntity = new TableEntity(category, OrderId)
     {
-        Console.WriteLine("The key is {0} and the value is {1}", item.Key, item.Value);
-    }
+        { "quantity", quantity }//add extra values
+    };
+    tableClient.AddEntity(tableEntity);
 
+    Console.WriteLine("Added entity");
 }
 
+AddEntity("1", "Laptop", 200);
+AddEntity("2", "PC", 200);
+AddEntity("3", "Laptop", 400);
+AddEntity("4", "Muebles", 100);
 
 
-//BLOB LEASE sirve para que solo una persona pueda modificar un blob a la ves
 
-await AcquireLease();
+//OBTENER ENTIDADES
 
-async Task AcquireLease()
+void GetEntities(string category)
 {
-    string connectionString = "DefaultEndpointsProtocol=https;AccountName=appstore20237;AccountKey=Lsk9U/W+AyCqq/x2TEqnJ1A81O3/vnGtSKlfIKQmbvA4quEU2xXilwGWdiGCciEa2CZftHPsiJIs+AStsLrAQQ==;EndpointSuffix=core.windows.net";
-    string containerName = "scripts";
-    string blobName = "avatar22.png";
+    TableClient tableClient = new TableClient(connectionString, tableName);
+    Pageable<TableEntity> results = tableClient.Query<TableEntity>(entity => entity.PartitionKey == category);
 
-    BlobClient blobClient = new BlobClient(connectionString, containerName,blobName);
-
-    BlobLeaseClient blobLeaseClient = blobClient.GetBlobLeaseClient();
-
-    TimeSpan leaseTiem = new TimeSpan(0, 0, 1, 00);
-
-    Response<BlobLease> response = await blobLeaseClient.AcquireAsync(leaseTiem);
-
-    Console.WriteLine("Leae id is {0}", response.Value.LeaseId);
+    foreach (var ent in results)
+    {
+        Console.WriteLine(ent.RowKey);
+        Console.WriteLine("Quantity IS {0}",ent.GetInt32("quantity"));
+    }
 }
-*/
+
+
+GetEntities("Laptop");
+
+
+//BORRAR ENTIDADES
+
+
+void DeleteEntity(string category,string orderID)
+{
+    TableClient tableClient = new TableClient(connectionString, tableName);
+    tableClient.DeleteEntity(category, orderID);
+
+    Console.WriteLine("Entity deleted");
+
+}
+
+DeleteEntity("Laptop", "1");
+
+
+//ACTUALIZR ENTIDADES
+
+void UpdateEntity(string category, string orderID, int quantity)
+{
+    // Let's first get the entity that we want to update
+    TableClient tableClient = new TableClient(connectionString, tableName);
+    Order order = tableClient.GetEntity<Order>(category, orderID);
+    order.quantity = quantity;
+
+    tableClient.UpdateEntity<Order>(order, ifMatch: ETag.All, TableUpdateMode.Replace);
+
+    Console.WriteLine("Entity updated");
+}
